951ce020fd84fa6c74bd0e9192daad106827bc55
TebogoLegoabe <tebogolegoabe5@gmail.com>
2024-01-03 21:59:24 +0200

    LFU Caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Create MRUCache class that inherits from BaseCaching"""
+""" Create LFUCache class that inherits from BaseCaching """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class MRUCache(BaseCaching):
-    """A caching system with LFU eviction"""
+class LFUCache(BaseCaching):
+    """LFU Caching"""
 
     def __init__(self):
-        """ Initialize MRUCache """
-        self.stack = []
+        """initialize"""
+        self.queue = []
+        self.lfu = {}
         super().__init__()
 
     def put(self, key, item):
@@ -20,26 +21,34 @@ class MRUCache(BaseCaching):
             item: The item to store.
         """
         if key and item:
-            if self.cache_data.get(key):
-                self.stack.remove(key)
-            while len(self.stack) >= self.MAX_ITEMS:
-                delete = self.stack.pop()
+            if (len(self.queue) >= self.MAX_ITEMS and
+                    not self.cache_data.get(key)):
+                delete = self.queue.pop(0)
+                self.lfu.pop(delete)
                 self.cache_data.pop(delete)
                 print('DISCARD: {}'.format(delete))
-            self.stack.append(key)
+
+            if self.cache_data.get(key):
+                self.queue.remove(key)
+                self.lfu[key] += 1
+            else:
+                self.lfu[key] = 0
+
+            insert_index = 0
+            while (insert_index < len(self.queue) and
+                   not self.lfu[self.queue[insert_index]]):
+                insert_index += 1
+            self.queue.insert(insert_index, key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Retrieve an item from the cache.
-
-        Args:
-            key: The key of the item to retrieve.
-
-        Returns:
-            The item associated with the key, or None if the key is not found.
-        """
+        """Return the value associated with the given key"""
         if self.cache_data.get(key):
-            self.stack.remove(key)
-            self.stack.append(key)
+            self.lfu[key] += 1
+            if self.queue.index(key) + 1 != len(self.queue):
+                while (self.queue.index(key) + 1 < len(self.queue) and
+                       self.lfu[key] >=
+                       self.lfu[self.queue[self.queue.index(key) + 1]]):
+                    self.queue.insert(self.queue.index(key) + 1,
+                                      self.queue.pop(self.queue.index(key)))
         return self.cache_data.get(key)
