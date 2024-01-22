2b19757c234f627125372e1148fabc469cf55948
TebogoLegoabe <tebogolegoab5@gmail.com>
2024-01-22 18:44:48 +0200

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index() -> str:
+    """home index"""
     return render_template('imdex.html')
 
 if __name__ == '__main__':
